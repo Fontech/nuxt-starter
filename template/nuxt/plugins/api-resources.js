1b00de2aba270a5e606ca701fa57bf47848d5f9b
sather33 <court.dream3@gmail.com>
2020-01-16 11:51:43 +0800

    remove all semicolon

--- template/nuxt/plugins/api-resources.js
+++ template/nuxt/plugins/api-resources.js
@@ -1,52 +1,52 @@
 const getResource = (action) => {
-  const actionArray = action.toLowerCase().split('_');
+  const actionArray = action.toLowerCase().split('_')
   if (actionArray.length !== 2) {
-    return null;
+    return null
   }
-  const [model, method] = actionArray;
+  const [model, method] = actionArray
   try {
-    return require(`~/config/api/${model}/${method}`);
-  } catch(error) {
-    return null;
+    return require(`~/config/api/${model}/${method}`)
+  } catch (error) {
+    return null
   }
-};
+}
 
 class Parser {
-  constructor(configs) {
-    this.configs = configs;
+  constructor (configs) {
+    this.configs = configs
   }
-  parse(data, type) {
-    const isParseToQuery = type === 'query';
+  parse (data, type) {
+    const isParseToQuery = type === 'query'
     return Object.keys(data).reduce((result, key) => {
-      const { default: defaultValue, required } = data[key];
-      const value = this.configs[key];
+      const { default: defaultValue, required } = data[key]
+      const value = this.configs[key]
       if ((value || required) && !isParseToQuery) {
-        result[key] = value || defaultValue;
+        result[key] = value || defaultValue
       }
-      return isParseToQuery ? `${result}${result ? '&' : '?'}${key}=${value || defaultValue}` : result;
-    }, isParseToQuery ? '' : {});
+      return isParseToQuery ? `${result}${result ? '&' : '?'}${key}=${value || defaultValue}` : result
+    }, isParseToQuery ? '' : {})
   }
 }
 
 const transResource = (resource, configs) => {
-  const { method, path, headers, query, params } = resource;
-  const parser = new Parser(configs);
+  const { method, path, headers, query, params } = resource
+  const parser = new Parser(configs)
   return {
     method,
     path: path + parser.parse(query, 'query'),
     headers: parser.parse(headers),
-    params: parser.parse(params),
-  };
-};
+    params: parser.parse(params)
+  }
+}
 
-export default function({ $axios }, inject) {
+export default function ({ $axios }, inject) {
   const api = (action, configs) => {
-    const resource = getResource(action);
+    const resource = getResource(action)
     if (!resource) {
-      return Promise.reject('Resource not found!');
+      return Promise.reject(new Error('Resource not found!'))
     }
-    const { method, path, headers, params } = transResource(resource, configs);
-    return $axios(path, { data: params, method, headers });
-  };
-  inject('api', api);
+    const { method, path, headers, params } = transResource(resource, configs)
+    return $axios(path, { data: params, method, headers })
+  }
+  inject('api', api)
 }
