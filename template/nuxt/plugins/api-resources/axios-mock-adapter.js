41df0e04f6cfc2c32fe25ed9d6b583d0173eda4d
sather33 <court.dream3@gmail.com>
2020-01-17 15:23:05 +0800

    add ActionAdapter

--- template/nuxt/plugins/api-resources/axios-mock-adapter.js
+++ template/nuxt/plugins/api-resources/axios-mock-adapter.js
@@ -1,8 +1,28 @@
 import MockAdapter from 'axios-mock-adapter'
+import apiDefinitions from '~/definitions/api'
+
+class ActionAdapter {
+  constructor (definitions, mockAdapter) {
+    this.definitions = definitions
+    this.mockAdapter = mockAdapter
+  }
+  buildPath (path) {
+    return path.replace(/{(\w+?)}/g, '1')
+  }
+  mock (action) {
+    const { method, path, response } = this.definitions[action]
+    const mockMethod = method[0].toUpperCase() + method.slice(1).toLowerCase()
+    const mockPath = this.buildPath(path)
+    this.mockAdapter[`on${mockMethod}`](mockPath).reply(200, response)
+  }
+}
 
 export default ({ $axios, app }) => {
   if (app.context.env.useMockApi) {
-    const mock = new MockAdapter($axios)
-    mock.onPost('/event/index').reply(200, { data: [1, 2, 3] })
+    const mockAdapter = new MockAdapter($axios)
+    for (const action in apiDefinitions) {
+      const actionAdapter = new ActionAdapter(apiDefinitions, mockAdapter)
+      actionAdapter.mock(action)
+    }
   }
 }
