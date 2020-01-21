71b1103bb2b865b43a9c1b5ed1ca1cb8429a2aaf
sather33 <court.dream3@gmail.com>
2020-01-21 16:31:02 +0800

    rename to AxiosMockAdapterProxy and remove definitions

--- template/nuxt/plugins/api-resources/axios-mock-adapter.js
+++ template/nuxt/plugins/api-resources/axios-mock-adapter.js
@@ -1,13 +1,12 @@
-import MockAdapter from 'axios-mock-adapter'
+import AxiosMockAdapter from 'axios-mock-adapter'
 import apiDefinitions from '~/definitions/api'
 
-class ActionDecorator {
-  constructor (definitions, mockAdapter) {
-    this.definitions = definitions
-    this.mockAdapter = mockAdapter
+class AxiosMockAdapterProxy {
+  constructor (axiosMockAdapter) {
+    this.axiosMockAdapter = axiosMockAdapter
   }
-  mock (action) {
-    const { method, path, response } = this.definitions[action]
+  mock (definitions) {
+    const { method, path, response } = definitions
     const onWhichMethod = `on${method.replace(/^(.)/, match => match.toUpperCase())}`
     this.mockAdapter[onWhichMethod](path).reply(200, response)
   }
@@ -17,9 +16,9 @@ export default ({ $axios, app }) => {
   if (!app.context.env.USE_MOCK_API) {
     return
   }
-  const mockAdapter = new MockAdapter($axios)
-  const actionDecorator = new ActionDecorator(apiDefinitions, mockAdapter)
+  const axiosMockAdapter = new AxiosMockAdapter($axios)
+  const axiosMockAdapterProxy = new AxiosMockAdapterProxy(axiosMockAdapter)
   for (const action in apiDefinitions) {
-    actionDecorator.mock(action)
+    axiosMockAdapterProxy.mock(apiDefinitions[action])
   }
 }
